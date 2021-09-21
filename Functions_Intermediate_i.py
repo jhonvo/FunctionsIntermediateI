4faf5afe756a668465b3e60ebb8ecbf637eee56d
jhonvo <jhon26victor@gmail.com>
2021-09-21 13:53:14 -0500

    Uploading solutions

---
+++ Functions_Intermediate_i.py
@@ -0,0 +1,66 @@
+#1 Update Values in Dictionaries and Lists
+
+x = [ [5,2,3], [10,8,9] ] 
+students = [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 10, 'y': 20} ]
+
+# Change the value 10 in x to 15. Once you're done, x should now be [ [5,2,3], [15,8,9] ]
+x[1][0] = 15
+print(x)
+
+# Change the last_name of the first student from 'Jordan' to 'Bryant'
+students[0]['last_name'] = 'Bryant'
+print(students)
+
+# In the sports_directory, change 'Messi' to 'Andres'
+sports_directory['soccer'][0] = 'Andres'
+print(sports_directory)
+
+#Change the value 20 in z to 30
+z[0]['y'] = 30
+print(z)
+
+#2 Iterate Through a List of Dictionaries: Create a function iterateDictionary(some_list) that, given a list of dictionaries, the function loops through each dictionary in the list and prints each key and the associated value. For example, given the following list:
+
+def iterateDictionary(value):
+    for x in value:
+        for y in x:
+            print (f'{y} - {x[y]}')
+
+students = [
+        {'first_name':  'Michael', 'last_name' : 'Jordan'},
+        {'first_name' : 'John', 'last_name' : 'Rosales'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+iterateDictionary(students) 
+
+#3 Get Values From a List of Dictionaries - 
+
+def iterateDictionary2(key_name,list):
+    for x in list:
+        print(x[key_name])
+
+iterateDictionary2('first_name',students) 
+iterateDictionary2('last_name',students) 
+
+#4 Iterate Through a Dictionary with List Values
+
+def printInfo(some_dict):
+    for x in some_dict:
+        print(f'{len(some_dict[x])} {x.upper()}')
+        for i in range (len(some_dict[x])):
+            print(some_dict[x][i])
+
+dojo = {
+   'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+   'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+printInfo(dojo)
